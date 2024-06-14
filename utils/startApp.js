988310440c1f628b7d53fe4ef1bcf5e52fc6477f
Dustin L <lawson.dustin12@gmail.com>
2024-06-13 19:35:45 -0500

    added a bunch of pages and renderToDom function and domBuilder

---
+++ utils/startApp.js
@@ -0,0 +1,14 @@
+import domBuilder from '../shared/domBuilder';
+// import navBar from '../shared/navBar';
+import logoutButton from '../components/logoutButton';
+
+const startApp = () => {
+  domBuilder();
+  // domEvents(user);
+  // formEvents(user);
+  // navBar();
+  // navigationEvents(user);
+  logoutButton();
+};
+
+export default startApp;
