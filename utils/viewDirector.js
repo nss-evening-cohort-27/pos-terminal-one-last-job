bb1c997c69bed88e1810d3ab87c1c49e5dbb2ec2
Dustin L <lawson.dustin12@gmail.com>
2024-06-13 21:07:48 -0500

    added navbar

--- utils/viewDirector.js
+++ utils/viewDirector.js
@@ -1,15 +1,15 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import logoutButton from '../components/logoutButton';
 import client from './client';
 import welcomePage from '../pages/loginPage';
+import startApp from './startApp';
 
 const ViewDirectorBasedOnUserAuthStatus = () => {
   firebase.initializeApp(client);
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // person is logged in do something...
-      logoutButton();
+      startApp();
     } else {
       // person is NOT logged in
       welcomePage();
