e2764ee82324c3309f17a81ad8340b6e8254150d
jmmyfam <pham_j@ymail.com>
2024-06-15 12:29:24 -0500

    view orders button landing page working

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,10 +1,16 @@
+import { getOrder } from '../api/orderData';
 import addOrderForm from '../components/forms/addOrderForm';
+import { showOrderCards } from '../pages/orders';
 
 const domEvents = () => {
   // landing page events
   document.querySelector('#main-container').addEventListener('click', (e) => {
     if (e.target.id.includes('landing-page-create-an-order-btn')) {
       addOrderForm();
+    }
+
+    if (e.target.id.includes('landing-page-view-orders-btn')) {
+      getOrder().then(showOrderCards);
     }
   });
 };
