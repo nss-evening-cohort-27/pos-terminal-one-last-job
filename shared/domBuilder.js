11c2aee612781121ef3356e5d53a41a89e348830
Dustin L <lawson.dustin12@gmail.com>
2024-06-15 12:16:42 -0500

    added domevent for create order

--- shared/domBuilder.js
+++ shared/domBuilder.js
@@ -3,12 +3,12 @@ import renderToDom from '../utils/renderToDom';
 const domBuilder = () => {
   const domString = `
   <div id="navigation"></div>
-  <div id="main-container">
+  <div id="main-container"></div>
     <div id="add-button"></div>
     <div id="form-container"></div>
     <div id="orderCards"></div>
     <div id="view"></div>
-  </div>`;
+  `;
 
   renderToDom('#app', domString);
 };
--- utils/startApp.js
+++ utils/startApp.js
@@ -2,15 +2,16 @@ import domBuilder from '../shared/domBuilder';
 import navBar from '../shared/navBar';
 import logoutButton from '../components/logoutButton';
 import landingPage from '../pages/landingPage';
+import domEvents from '../events/domEvents';
 
 const startApp = () => {
   domBuilder();
-  // domEvents(user);
+  domEvents();
   // formEvents(user);
+  landingPage();
   navBar();
   // navigationEvents(user);
   logoutButton();
-  landingPage();
 };
 
 export default startApp;
